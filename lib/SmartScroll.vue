349d199bd875066110b12095457bdfc37d51a460
avesheva <shevochkandrey@gmail.com>
2022-12-10 17:29:02 +0200

    Scroll items visibility checking basic implementation.

--- lib/SmartScroll.vue
+++ lib/SmartScroll.vue
@@ -1,10 +1,61 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { delay } from './utils/helpers'
+
+const props = defineProps<{
+  id: string,
+  scrollDelay?: number,
+}>()
+
+let wrapperBlock: HTMLElement | null = null
+let wrapperClientRect: DOMRect | null = null
+
+const init = () => {
+  wrapperBlock = document.getElementById(props.id)
+
+  if (!wrapperBlock) throw Error(`Can't find element with id "${props.id}"`)
+
+  wrapperClientRect = wrapperBlock.getBoundingClientRect()
+}
+
+const checkItemsVisibility = (wrapper: HTMLElement) => {
+  if (!wrapperClientRect) return
+
+  for (const item of wrapper.children) {
+    const itemCoordinates = item.getBoundingClientRect()
+    const itemMiddle = item.clientHeight / 2
+
+    if (
+      (itemCoordinates.bottom - itemMiddle <= wrapperClientRect.bottom)
+      && (itemCoordinates.bottom >= wrapperClientRect.top)
+    ) {
+      item.setAttribute('data-is-visible', 'true')
+    } else {
+      item.setAttribute('data-is-visible', 'false')
+    }
+  }
+}
+
+const scrollHandler = () => {
+  wrapperBlock && checkItemsVisibility(wrapperBlock)
+}
+
+const delayedScrollHandler = delay(scrollHandler, props.scrollDelay || 300)
+
+onMounted(() => {
+  init()
+  wrapperBlock && checkItemsVisibility(wrapperBlock)
+})
 
 </script>
 
 <template>
-  <div>
-    <h2>Smart Scroll component</h2>
+  <div
+    :id="id"
+    style="overflow-y: auto"
+    @scroll="delayedScrollHandler"
+  >
+    <slot />
   </div>
 </template>
 
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <smart-scroll />
+    <smart-scroll id="itemsWrapper">
+      <div
+        v-for="number in 7"
+        class="item"
+        :key="number"
+      >
+        Item: {{ number }}
+      </div>
+    </smart-scroll>
   </div>
 </template>
 
@@ -10,5 +18,25 @@ import SmartScroll from '../lib'
 </script>
 
 <style scoped>
+#itemsWrapper {
+  border: solid;
+  max-height: 355px;
+}
 
+*[data-is-visible="true"] {
+  background: white;
+  color: #242424;
+  transition: background-color 2s;
+}
+*[data-is-visible="false"] {
+  background: #242424;
+  color: white;
+  transition: background-color 2s;
+}
+
+.item {
+  padding: 2rem;
+  font-size: 2rem;
+  outline: solid 1px;
+}
 </style>
