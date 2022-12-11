66bad6212537bf60c1e2b8f2f0f43a79175f34dc
avesheva <shevochkandrey@gmail.com>
2022-12-11 23:01:52 +0200

    Add 'scroll' event. Props changes.

--- lib/SmartScroll.vue
+++ lib/SmartScroll.vue
@@ -2,13 +2,25 @@
 import { onMounted } from 'vue'
 import { delay } from './utils/helpers'
 
-const props = defineProps<{
-  id: string,
+interface Props {
+  id?: string,
   scrollDelay?: number,
-}>()
+}
+const props = withDefaults(defineProps<Props>(), {
+  id: 'smartScrollContainer',
+  scrollDelay: 300,
+})
+
+const emit = defineEmits(['scroll', 'visibility-change'])
 
 let wrapperBlock: HTMLElement | null = null
 let wrapperClientRect: DOMRect | null = null
+
+const emitVisibilityChange = () => {
+  console.log('EMIT visibility change ... ')
+}
+
+const delayedEmitVisibilityChange = delay(emitVisibilityChange, 1000)
 
 const init = () => {
   wrapperBlock = document.getElementById(props.id)
@@ -24,23 +36,30 @@ const checkItemsVisibility = (wrapper: HTMLElement) => {
   for (const item of wrapper.children) {
     const itemCoordinates = item.getBoundingClientRect()
     const itemMiddle = item.clientHeight / 2
+    const isVisible = item.getAttribute('data-is-visible')
 
     if (
       (itemCoordinates.bottom - itemMiddle <= wrapperClientRect.bottom)
       && (itemCoordinates.bottom >= wrapperClientRect.top)
     ) {
-      item.setAttribute('data-is-visible', 'true')
+      if (!isVisible || isVisible === 'false') {
+        item.setAttribute('data-is-visible', 'true')
+        delayedEmitVisibilityChange()
+      }
     } else {
-      item.setAttribute('data-is-visible', 'false')
+      if (!isVisible || isVisible === 'true') {
+        item.setAttribute('data-is-visible', 'false')
+        delayedEmitVisibilityChange()
+      }
     }
   }
 }
 
 const scrollHandler = () => {
   wrapperBlock && checkItemsVisibility(wrapperBlock)
+  emit('scroll')
 }
-
-const delayedScrollHandler = delay(scrollHandler, props.scrollDelay || 300)
+const delayedScrollHandler = delay(scrollHandler, props.scrollDelay)
 
 onMounted(() => {
   init()
