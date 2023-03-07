c885b8a65446946a798c5bc483face81f66bcb40
avesheva <shevochkandrey@gmail.com>
2023-03-07 10:04:18 +0200

    Changed logic to version with Intersection Observer

--- lib/SmartScroll.vue
+++ lib/SmartScroll.vue
@@ -1,105 +1,65 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import { delay } from './utils/helpers'
 
 export interface Props {
   id?: string,
   scrollDelay?: number,
-  onlyVisible?: boolean,
-  deleteViewed?: boolean,
-  itemAttribute?: string,
+  rootMargin?: string,
+  threshold?: number | number[]
+}
+
+export interface IIntersectionData {
+  scrollDirection: 'up' | 'down',
+  entries: IntersectionObserverEntry[],
 }
 
 const props = withDefaults(defineProps<Props>(), {
   id: 'smartScrollContainer',
-  scrollDelay: 300,
-  onlyVisible: false,
-  deleteViewed: false,
-  itemAttribute: '',
+  scrollDelay: 0,
+  rootMargin: '',
+  threshold: 0,
 })
 
-const emit = defineEmits(['scroll', 'items-delete'])
+const emit = defineEmits(['intersect'])
 
-let wrapperBlock: HTMLElement | null = null
-let wrapperClientRect: DOMRect | null = null
-let scrollTop = 0
-let deleting = false
+let intersectionObserver: IntersectionObserver | null = null
+let lastY = 0
 
 const init = () => {
-  wrapperBlock = document.getElementById(props.id)
+  const wrapper = document.getElementById(props.id)
 
-  if (wrapperBlock) {
-    wrapperClientRect = wrapperBlock.getBoundingClientRect()
-    scrollTop = wrapperBlock.scrollTop
-  }
-}
+  if (!wrapper) return
+  lastY = wrapper.scrollTop
 
-const checkItemsVisibility = (wrapper: HTMLElement) => {
-  if (!wrapperClientRect) return
-
-  for (const item of wrapper.children) {
-    const itemCoordinates = item.getBoundingClientRect()
-    const itemMiddle = item.clientHeight / 2
-    const isVisible = item.getAttribute('data-is-visible')
-
-    if (
-      (itemCoordinates.bottom - itemMiddle <= wrapperClientRect.bottom)
-      && (itemCoordinates.bottom - itemMiddle >= wrapperClientRect.top)
-    ) {
-      if (!isVisible || isVisible === 'false') {
-        item.setAttribute('data-is-visible', 'true')
-      }
-    } else if (!props.onlyVisible) {
-      if (!isVisible || isVisible === 'true') {
-        item.setAttribute('data-is-visible', 'false')
-      }
+  const observerCallBack = delay((entries: IntersectionObserverEntry[]): void => {
+    const data: IIntersectionData = {
+      scrollDirection: lastY > wrapper.scrollTop ? 'up' : 'down',
+      entries,
     }
+
+    lastY = wrapper.scrollTop
+
+    emit('intersect', data)
+  }, props.scrollDelay)
+
+  intersectionObserver = new IntersectionObserver(observerCallBack, {
+    root: wrapper,
+    threshold: props.threshold,
+    rootMargin: props.rootMargin,
+  })
+
+  for (const child of wrapper.children) {
+    intersectionObserver.observe(child)
   }
 }
-
-const deleteViewedItems = (wrapper: HTMLElement) => {
-  const deletedItems = []
-
-  for (const item of wrapper.children) {
-    const isVisible = item.getAttribute('data-is-visible')
-
-    if (isVisible === 'false') {
-      deleting = true
-      item.remove()
-
-      if (props.itemAttribute) {
-        deletedItems.push(item.getAttribute(props.itemAttribute))
-      }
-    } else {
-      break
-    }
-  }
-
-  deletedItems.length > 0 && emit('items-delete', deletedItems)
-}
-
-const scrollHandler = () => {
-  if (deleting) {
-    deleting = false
-    return
-  }
-
-  if (wrapperBlock) {
-    const direction: 'up' | 'down' = scrollTop < wrapperBlock.scrollTop ? 'down' : 'up'
-
-    checkItemsVisibility(wrapperBlock)
-    scrollTop = wrapperBlock.scrollTop
-
-    emit('scroll', direction)
-
-    props.deleteViewed && deleteViewedItems(wrapperBlock)
-  }
-}
-const delayedScrollHandler = delay(scrollHandler, props.scrollDelay)
 
 onMounted(() => {
   init()
-  wrapperBlock && checkItemsVisibility(wrapperBlock)
+})
+
+onUnmounted(() => {
+  intersectionObserver?.disconnect()
 })
 
 </script>
@@ -108,7 +68,6 @@ onMounted(() => {
   <div
     :id="id"
     style="overflow-y: auto"
-    @scroll="delayedScrollHandler"
   >
     <slot />
   </div>
--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -1,7 +1,7 @@
-export const delay = (f: (args: UIEvent) => void, t: number) => {
+export const delay = (f: (args: IntersectionObserverEntry[]) => void, t: number) => {
   let timeoutId: number | null = null
 
-  return (args: UIEvent) => {
+  return (args: IntersectionObserverEntry[]) => {
     if (timeoutId) clearTimeout(timeoutId)
 
     timeoutId = setTimeout(() => { f(args) }, t) as unknown as number
